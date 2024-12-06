99ab29a00f9ab40ebd4daea39be11199f5568fa3
Novish Khalil <novishkhalil2@hotmail.com>
2024-12-07 01:26:23 +0500

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function myproject(){
+  return (
+    <div className="bg-blue-800 text-5xl pt-4 font-bold text-center text-white h-20 w-full ">
+      <h1><u>Hello World</u></h1>
+    </div>
+  )
+}
